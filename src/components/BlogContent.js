fc4adc2f3e166b80dbf7f20a281126dd6d0ae322
tugbacanbaz <tugbacanbaz.pi@gmail.com>
2022-12-13 21:52:04 +0300

    Complited Assignment

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  console.log(props);
+
+  if (!props.isPublished) {
+    
+    return null;
+    
+  } else {
+    
+    return (
+      <div>
+        <h1>{props.articleText}</h1>
+        <p>{props.minutesToRead} minutes to read</p>
+      </div>
+    );
+  }
 }
 
 export default BlogContent;
